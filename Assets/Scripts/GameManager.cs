e7059862370f90fa7fe47761a148d01e4c588933
TimtamD <letsgetgaming097@gmail.com>
2025-05-27 16:13:52 +1000

    Adding all
    Added Isaac
    Fixed most visual errors
    Added some scripts

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,9 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class GameManager : MonoBehaviour{
+public class GameManager : MonoBehaviour
+{
     public GameObject player;
-   
+
     // Pickup and Level Completion Logic
     public int currentPickups = 0;
     public int maxPickups = 5;
@@ -16,6 +17,11 @@ public class GameManager : MonoBehaviour{
     public AudioSource[] audioSources;
     public float audioProximity = 15.0f;
 
+    public Transform door;
+    public float doorProximity = 10.0f;
+    private Animation[] doorAnimations;
+    private Dictionary<Animation, bool> hasPlayedMap = new Dictionary<Animation, bool>();
+
     // Update is called once per frame
     void Update()
     {
@@ -24,26 +30,49 @@ public class GameManager : MonoBehaviour{
         PlayAudioSamples();
     }
 
-    private void LevelCompleteCheck(){
+    private void LevelCompleteCheck()
+    {
         if (currentPickups >= maxPickups)
             levelComplete = true;
         else
             levelComplete = false;
     }
 
-    private void UpdateGUI(){
+    private void UpdateGUI()
+    {
         pickupText.text = "Pickups: " + currentPickups + "/" + maxPickups;
     }
 
-    private void PlayAudioSamples(){
-        for (int i = 0; i < audioSources.Length; i++){
-            if (audioSources[i] !=null){
-                if(Vector3.Distance(player.transform.position, audioSources[i].transform.position) <= audioProximity){
-                    if(!audioSources[i].isPlaying){
-                    audioSources[i].Play();
+    private void PlayAudioSamples()
+    {
+        for (int i = 0; i < audioSources.Length; i++)
+        {
+            if (audioSources[i] != null)
+            {
+                if (Vector3.Distance(player.transform.position, audioSources[i].transform.position) <= audioProximity)
+                {
+                    if (!audioSources[i].isPlaying)
+                    {
+                        audioSources[i].Play();
                     }
                 }
             }
+        }
+    }
+    
+    private void AutomaticDoor()
+    {
+        foreach (Animation anim in doorAnimations)
+        {
+            float distance = Vector3.Distance(player.transform.position, anim.transform.position);
+
+            if (distance <= doorProximity && !hasPlayedMap[anim])
+            {
+                anim.Play();
+                hasPlayedMap[anim] = true;
+            }
+            else if (distance > doorProximity && hasPlayedMap[anim])
+                hasPlayedMap[anim] = false;
         }
     }
 }
--- Assets/Tamsyn Douglas/TD Third Person Controller/TD PlayerMovement_TD.cs
+++ Assets/Tamsyn Douglas/TD Third Person Controller/TD PlayerMovement_TD.cs
@@ -2,19 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour{
+public class TD_PlayerMovement : MonoBehaviour
+{
 
     public Animator anim;
 
     public float rotSpeed = 10;
 
+    private bool isSitting = false;
+
     // Start is called before the first frame update
-    void Start(){
-        
+    void Start()
+    {
+
     }
 
     // Update is called once per frame
-    void Update(){
+    void Update()
+    {
 
         ForwardMovement();
 
@@ -22,55 +27,97 @@ public class PlayerMovement : MonoBehaviour{
 
         Actions();
 
+        Sitting();
+
     }
 
-    private void ForwardMovement(){
-        if(Input.GetKey("w")){
+    private void ForwardMovement()
+    {
+        if (Input.GetKey("w"))
+        {
             anim.SetBool("Walking", true);
-            if (Input.GetKey(KeyCode.Space)){
+            if (Input.GetKey(KeyCode.Space))
+            {
                 anim.SetBool("Jump", true);
-            } else{
+            }
+            else
+            {
                 anim.SetBool("Jump", false);
             }
-            if (Input.GetKey(KeyCode.LeftShift)){
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
                 anim.SetBool("Running", true);
-                if (Input.GetKey(KeyCode.Space)){
+                if (Input.GetKey(KeyCode.Space))
+                {
                     anim.SetBool("RunJump", true);
-                } else{
+                }
+                else
+                {
                     anim.SetBool("RunJump", false);
                 }
-            } else{
+            }
+            else
+            {
                 anim.SetBool("Running", false);
             }
-        if(Input.GetKey(KeyCode.Space)){
-            anim.SetBool("Jump", true);
+            if (Input.GetKey(KeyCode.Space))
+            {
+                anim.SetBool("Jump", true);
+            }
         }
-        } else if (Input.GetKeyUp("w")) {
+        else if (Input.GetKeyUp("w"))
+        {
             anim.SetBool("Walking", false);
             anim.SetBool("Running", false);
             anim.SetBool("Jump", false);
         }
-        
+
     }
 
-    private void Turning(){
-        if (Input.GetKey("a")) {
+    private void Turning()
+    {
+        if (Input.GetKey("a"))
+        {
             transform.Rotate(0, -rotSpeed * 15 * Time.deltaTime, 0, Space.World);
             anim.SetBool("Turn Left", true);
-        } else if (Input.GetKey("d")) {
+        }
+        else if (Input.GetKey("d"))
+        {
             transform.Rotate(0, rotSpeed * 15 * Time.deltaTime, 0, Space.World);
             anim.SetBool("Turn Right", true);
-        } else {
+        }
+        else
+        {
             anim.SetBool("Turn Left", false);
             anim.SetBool("Turn Right", false);
         }
     }
 
-    private void Actions(){
-        if(Input.GetKeyDown("e")){
+    private void Actions()
+    {
+        if (Input.GetKeyDown("e"))
+        {
             anim.SetBool("Waving", true);
-        } else if(Input.GetKeyUp("e")){
+        }
+        else if (Input.GetKeyUp("e"))
+        {
             anim.SetBool("Waving", false);
+        }
+    }
+    
+    private void Sitting()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            isSitting = !isSitting; // Toggle sitting state
+            anim.SetBool("Sitting", isSitting);
+        }
+
+        // Cancel sitting on any other key press
+        if (isSitting && Input.anyKeyDown && !Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            isSitting = false;
+            anim.SetBool("Sitting", false);
         }
     }
 }
