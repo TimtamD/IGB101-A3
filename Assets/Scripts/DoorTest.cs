20700aae5c384fd15eb406dc2a4a76eb0b23e149
TimtamD <letsgetgaming097@gmail.com>
2025-05-27 13:21:24 +1000

    Delete Unecessary Changes

---
+++ Assets/Scripts/DoorTest.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTest : MonoBehaviour
+{   
+    Animation animation;
+    // Use this for initialization
+    void Start () {
+    animation = GetComponent<Animation>();
+    }
+    // Update is called once per frame
+    void Update () {
+    if (Input.GetKeyDown("f"))
+    animation.Play();
+    }
+
+}
