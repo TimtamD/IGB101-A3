3cdb7f081500cc765e5fab574df8cb35194b1b7c
Rckstly <nishantatsingh@gmail.com>
2025-05-30 14:32:58 +1000

    my level almost finished

--- Assets/Scripts/Pickup.cs
+++ Assets/Scripts/Pickup.cs
@@ -2,21 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Pickup : MonoBehaviour{
+public class Pickup : MonoBehaviour
+{
+    private GameManager gameManager;
 
-    GameManager gameManager;
-
-    // Start is called before the first frame update
     void Start()
     {
-        gameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+        GameObject gm = GameObject.FindGameObjectWithTag("GameManager");
+        if (gm != null)
+        {
+            gameManager = gm.GetComponent<GameManager>();
+        }
+        else
+        {
+            Debug.LogError("GameManager not found! Make sure it's tagged 'GameManager'.");
+        }
     }
 
-    private void OnTriggerEnter(Collider otherObject)
+    private void OnTriggerEnter(Collider other)
     {
-        if(otherObject.transform.tag == "Player"){
-            gameManager.currentPickups += 1;
-            Destroy(this.gameObject);
+        if (other.CompareTag("Player"))
+        {
+            if (gameManager != null)
+            {
+                gameManager.currentPickups++;
+                Debug.Log("Pickup collected! Total: " + gameManager.currentPickups);
+                Destroy(gameObject);
+            }
         }
     }
 }
